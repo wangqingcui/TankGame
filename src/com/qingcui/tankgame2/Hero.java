40903f12442b8be0a9fff9a2711ce81c4d5a5496
wangqingcui <1032252940@qq.com>
2023-01-21 20:40:05 +0800

    tankgame02 start

---
+++ src/com/qingcui/tankgame2/Hero.java
@@ -0,0 +1,12 @@
+package com.qingcui.tankgame2;
+
+/**
+ * @author 王清脆
+ * @version 1.0
+ */
+//我的坦克
+public class Hero extends Tank {
+    public Hero(int x, int y) {
+        super(x, y);
+    }
+}
---
+++ src/com/qingcui/tankgame2/TankGame02.java
@@ -0,0 +1,22 @@
+package com.qingcui.tankgame2;
+
+import javax.swing.*;
+
+/**
+ * @author 王清脆
+ * @version 1.0
+ */
+public class TankGame02 extends JFrame {
+//    定义MyPanel
+    MyPanel mp=null;
+    public static void main(String[] args) {
+        TankGame02 tankGame01 = new TankGame02();
+    }
+   public TankGame02(){
+        mp=new MyPanel();
+        this.add(mp);//把面板（绘图区）
+       this.setSize(1000,750);
+       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       this.setVisible(true);
+   }
+}
